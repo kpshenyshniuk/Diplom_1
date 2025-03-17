c7e52a065f0a516852ca1995cd513e180b961440
kpshenyshniuk <k.pshenyshniuk@abcmobile.com>
2025-03-17 15:34:58 +0100

    исправил замечания

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -46,3 +46,39 @@ class TestBurger:
         burger.add_ingredient(ingredient2)
         expected_price = (2.00 * 2) + 1.00 + 1.50
         assert burger.get_price() == expected_price
+
+    def test_move_ingredient(self):
+        """Проверяем, что можно изменить порядок ингредиентов в бургере"""
+        burger = Burger()
+        ingredient1 = Mock(spec=Ingredient)
+        ingredient2 = Mock(spec=Ingredient)
+        ingredient3 = Mock(spec=Ingredient)
+        burger.add_ingredient(ingredient1)
+        burger.add_ingredient(ingredient2)
+        burger.add_ingredient(ingredient3)
+        burger.move_ingredient(0, 2)  # Перемещаем первый ингредиент на третье место
+
+        assert burger.ingredients == [ingredient2, ingredient3, ingredient1]
+
+    def test_burger_get_receipt(self):
+        """Проверяем, что чек бургера формируется корректно"""
+        burger = Burger()
+        bun = Mock(spec=Bun)
+        bun.get_name.return_value = "Sesame Bun"
+        burger.set_buns(bun)
+        ingredient1 = Mock(spec=Ingredient)
+        ingredient1.get_name.return_value = "Cheese"
+        ingredient1.get_type.return_value = "FILLING"
+        ingredient2 = Mock(spec=Ingredient)
+        ingredient2.get_name.return_value = "Lettuce"
+        ingredient2.get_type.return_value = "VEGGIE"
+        burger.add_ingredient(ingredient1)
+        burger.add_ingredient(ingredient2)
+        burger.get_price = Mock(return_value=5.50)
+        expected_receipt = """(==== Sesame Bun ====)
+= filling Cheese =
+= veggie Lettuce =
+(==== Sesame Bun ====)
+
+Price: 5.5"""
+        assert burger.get_receipt() == expected_receipt
