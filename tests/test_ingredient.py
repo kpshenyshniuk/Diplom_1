eab5559868162e9d16545e0f9a4f6322a5bcbf63
kpshenyshniuk <k.pshenyshniuk@abcmobile.com>
2025-03-17 15:30:31 +0100

    bсправления

--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -5,15 +5,20 @@ from ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
 
 class TestIngredient:
 
-    @pytest.mark.parametrize("ingredient_type, name, price", [
-        (INGREDIENT_TYPE_SAUCE, "Ketchup", 0.50),
-        (INGREDIENT_TYPE_SAUCE, "BBQ Sauce", 0.75),
-        (INGREDIENT_TYPE_FILLING, "Beef Patty", 3.00),
-        (INGREDIENT_TYPE_FILLING, "Chicken Patty", 2.50)
-    ])
-    def test_ingredient_returns_correct_values(self, ingredient_type, name, price):
-        """Проверяем, что Ingredient корректно возвращает тип, имя и цену"""
-        ingredient = Ingredient(ingredient_type, name, price)
+    @pytest.mark.parametrize("ingredient_type", [INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING])
+    def test_ingredient_get_type(self, ingredient_type):
+        """Проверяем, что get_type() возвращает правильный тип"""
+        ingredient = Ingredient(ingredient_type, "Test", 1.00)
         assert ingredient.get_type() == ingredient_type
+
+    @pytest.mark.parametrize("name", ["Ketchup", "BBQ Sauce", "Beef Patty", "Chicken Patty"])
+    def test_ingredient_get_name(self, name):
+        """Проверяем, что get_name() возвращает правильное имя"""
+        ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, name, 1.00)
         assert ingredient.get_name() == name
+
+    @pytest.mark.parametrize("price", [0.50, 0.75, 3.00, 2.50])
+    def test_ingredient_get_price(self, price):
+        """Проверяем, что get_price() возвращает правильную цену"""
+        ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, "Test", price)
         assert ingredient.get_price() == price
